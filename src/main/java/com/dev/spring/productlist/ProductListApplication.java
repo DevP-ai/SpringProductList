928682cfd57cfe278d58791bb6b7fc0918339362
Devajit <patar1447@gmail.com>
2025-04-06 16:25:11 +0530

    Application name change

--- src/main/java/com/dev/spring/productlist/ProductListApplication.java
+++ src/main/java/com/dev/spring/productlist/ProductListApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductlistApplication {
+public class ProductListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductlistApplication.class, args);
+		SpringApplication.run(ProductListApplication.class, args);
 	}
 
 }

530361f697a89b3b778aa6158facf6823bf49058
Devajit <patar1447@gmail.com>
2025-04-06 15:55:48 +0530

    Project Setup

---
+++ src/main/java/com/dev/spring/productlist/ProductlistApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring.productlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlistApplication.class, args);
+	}
+
+}
